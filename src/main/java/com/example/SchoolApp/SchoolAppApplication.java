8c1088f7e728267b13600918a32fe9808c08954b
siswarnava-sss <sinhaswarnava@gmail.com>
2022-06-30 21:15:51 +0530

    Initial commit

---
+++ src/main/java/com/example/SchoolApp/SchoolAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SchoolApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAppApplication.class, args);
+	}
+
+}
